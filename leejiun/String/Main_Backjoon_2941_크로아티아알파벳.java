6bf0078d355183657bfe5ea46a1531b46e5e005c
LEE JI UN <45230079+wldnswldnswl@users.noreply.github.com>
2020-12-13 14:02:07 +0900

    [이ㅣ우, refactor] Backjoon_2941_크로아티아알파벳

--- leejiun/String/Main_Backjoon_2941_크로아티아알파벳.java
+++ leejiun/String/Main_Backjoon_2941_크로아티아알파벳.java
@@ -1,11 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
 
-// https://www.acmicpc.net/problem/2941\
-/**
- * 아이디어: 변환 무ㅏ를 차으며 replace("창으
- *
- */
+// https://www.acmicpc.net/problem/2941
 public class Main_Backjoon_2941_크로아티아알파벳 {
 
 	public static void main(String[] args) throws Exception{
@@ -15,24 +13,11 @@ public class Main_Backjoon_2941_크로아티아알파벳 {
 		int dic_size = dic.length;
 		int cnt = 0;
 
-		while(input.length() > 0) {//
 			for (int i = 0; i < dic_size; i++) {
-				int startIndex = input.indexOf(dic[i]);
-				if(startIndex != -1) {
-					input = input.replaceFirst(dic[i], "&");
-					cnt++;
-					break;
-				}
-				if(i == dic_size - 1 && startIndex == -1) {
-					input = input.replace("&", "");
-					cnt += input.length();
-					input = "";
-				}
+					input = input.replace(dic[i], "&");
 			}
-			
-		}
 		
-		System.out.println(cnt);
+		System.out.println(input.length());
 			
 
 	}
